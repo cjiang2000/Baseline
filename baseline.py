798a0c20322a4b6a0537c86be515402bfe475d0e
Chris <cjiang2000@gmail.com>
2020-06-11 12:06:21 -0400

    Update

--- baseline.py
+++ baseline.py
@@ -7,16 +7,16 @@ from datetime import datetime
 #choose file
 path = easygui.fileopenbox()
 #read excel to dataframe
-data = pd.read_excel(path, sheet_name = 'Mapped AD IMS for testing-new', usecols = ['Milestone', 'ACIO', 'Application', "Start_Date_(Before_CPDs)", "Baseline_Start", "Status_Date"])
+data = pd.read_excel(path, sheet_name = 'Mapped AD IMS for testing-new', usecols = ['Milestone', 'ACIO', 'Application', 'Start_Date_(Before_CPDs)', 'Baseline_Start', 'Status_Date', 'PercentComp', 'Release1', 'GANTT RELEASE'])
 date = (data.at[1,'Status_Date']).strftime("%d_%b_%Y")
 #remove unnecessary data from the dataframe
 rowIndex = []
 for index, row in data.iterrows():
-	if row['Milestone'] == 'RELEASE' and row['ACIO'][0:1] == 'AD':
+	if row['Milestone'] == 'RELEASE' and row['ACIO'][0:2] == 'AD' and row['PercentComp'] != 1.0:
 		rowIndex.append(index)
-	elif row['Milestone'] == 'INITIATIVE' and row['ACIO'][0:1] != 'AD':
+	elif row['Milestone'] == 'INITIATIVE' and row['ACIO'][0:2] != 'AD':
 		rowIndex.append(index)
-	if row['Milestone'] == 'APPLICATION' and row['ACIO'][0:1] != 'AD':
+	if row['Milestone'] == 'APPLICATION' and row['ACIO'][0:2] != 'AD':
 		rowIndex.append(index)
 data = data.iloc[rowIndex]
 #Build dataframes
@@ -30,17 +30,20 @@ for index, row in data.iterrows():
 	start = row['Start_Date_(Before_CPDs)']
 	days = (now - start).days
 	if pd.isnull(base) and (days > 60):
-		baseline.append((row['ACIO'], row['Application'], days))
+		if row['Milestone'] == 'RELEASE':
+			baseline.append((row['ACIO'], row['Application'], row['Release1'], row['GANTT RELEASE'], days))
+		else:
+			baseline.append((row['ACIO'], row['Application'], float('NaN'), float('NaN'), days))
 
 for index, row in data.iterrows():
 	base = row['Baseline_Start']
 	start = row['Start_Date_(Before_CPDs)']
 	if not pd.isnull(base) and (base - start).days > 0:
-		late.append((row['ACIO'], row['Application'], days))
+		late.append((row['ACIO'], row['Application'], (base - start).days))
 
 
 #convert to dataframes
-baseline = pd.DataFrame(sorted(sorted(baseline,key=itemgetter(1)), key=itemgetter(0)), columns = ['ACIO', 'Application', 'Days Past Start Date'])
+baseline = pd.DataFrame(sorted(sorted(baseline,key=itemgetter(1)), key=itemgetter(0)), columns = ['ACIO', 'Application', 'Release', 'Gant Release', 'Days Past Start Date'])
 late = pd.DataFrame(sorted(sorted(late,key=itemgetter(1)), key=itemgetter(0)), columns = ['ACIO', 'Application', 'Days Past Baseline'])
 
 #write to excel
